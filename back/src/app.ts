8b64f6eafc69c5f825462863179317ceab307314
Filipe <filipeotaviomm@gmail.com>
2024-01-24 15:40:28 -0300

    feat: basic structure back-end

---
+++ back/src/app.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express, { Application } from "express";
+import { handleError } from "./errors";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use(handleError);
+
+export default app;
