bf1044d76b114b350df2d2e5eef3e5f9cef07c3d
Filipe <filipeotaviomm@gmail.com>
2024-09-26 11:26:35 -0700

    exclude back-nest

--- back-nest/src/modules/auth/jwt-auth.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable() //essa classe é utilizada em users.controller.ts
-export class JwtAuthGuard extends AuthGuard('jwt') {}
